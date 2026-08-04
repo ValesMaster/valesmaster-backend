7efb7c017c9c9783d42299ed420e2bd61393cde7
estebanc <esteban.oz220106@gmail.com>
2026-08-03 19:56:16 -0600

    feat(solicitudes): se anaden endpoints para consultar

--- src/routes/solicitudes.routes.ts
+++ src/routes/solicitudes.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { crearPresolicitud, validarPresolicitud, AprobarSolicitud } from "../controllers/solicitudes.controller";
+import {
+    crearPresolicitud,
+    validarPresolicitud,
+    aprobarSolicitud,
+    getPresolicitudes,
+    getSolicitudes,
+    getPresolicitudDetalle
+} from "../controllers/solicitudes.controller";
 
 const router = Router();
 
-router.post('/crear-presolicitud', crearPresolicitud);
-router.post('/validar-presolicitud/:id', validarPresolicitud);
-router.post('/aprobar-solicitud/:id', AprobarSolicitud);
+router.post('/crear', crearPresolicitud);
+router.post('/validar/:id', validarPresolicitud);
+router.post('/aprobar/:id', aprobarSolicitud);
+router.get('/obtener-solicitudes', getSolicitudes);
+router.get('/obtener-presolicitudes', getPresolicitudes);
+router.get('/detalle-presolicitud/:id', getPresolicitudDetalle);
 
 export default router;
